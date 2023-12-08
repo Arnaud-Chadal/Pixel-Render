e1fb6bee5a1988d3dac16e24ef33a26c80565190
Arnaud <chadal.arnaud2@gmail.com>
2023-12-09 00:42:00 +0100

    Continue apply_depth.py

--- apply_depth.py
+++ apply_depth.py
@@ -1,26 +1,44 @@
 from depth_layer import *
 
 path = "./image_To_Edit/"
+pathLayer = "./depth_Layer/"
 image = Image.open(path + "imageTest.png")
+layer = Image.open(pathLayer + "depthImage2.png")
 size = 10
 
 
 def resizer(img) :
     name = "test.png"
-    depthMap = getDepthMap(img)
+    depthMap = getDepthMap(layer)
     img2 = Image.new('RGBA', (img.width*size, img.height*size), (255,255,255,1))
     for i in range(img.height) :
         for j in range(img.width) :
-            color = img.getpixel((j,i))[:3]
+            color = img.getpixel((i, j))[:3]
+            north = False
+            south = False
+            west = False
+            east = False
+            if depthMap[i-1][j] > depthMap[i][j] :
+                north = True
+            if depthMap[i+1][j] > depthMap[i][j] :
+                south = True
+            if depthMap[i][j-1] > depthMap[i][j] :
+                west = True
+            if depthMap[i][j+1] > depthMap[i][j] :
+                east = True
             for m in range(size) :
-                for n in range(size) :
-                    if (i == 0 or j == 0 or i == img.height-1 or j == img.width-1) and (i == 0 and n == 0) or (j == 0 and m == 0) or (i == img.height-1 and n == size-1) or (j == img.width-1 and m == size-1) :
-                        img2.putpixel((j*size+m,i*size+n), (0, 0, 0))
-                    else : img2.putpixel((j*size+m,i*size+n), color)
+                for n in range(size) : 
+                    if north :
+                        img2.putpixel((j*size+m,i*size+n), (color[0]-(200-m*40), color[1]-(200-m*40), color[2]-(200-m*40)))
+                    if south :
+                        img2.putpixel((j*size+m,i*size+n), (color[0]-(200-(size-m)*40), color[1]-(200-(size-m)*40), color[2]-(200-(size-m)*40)))
+                    if west :
+                        img2.putpixel((j*size+m,i*size+n), (color[0]-(50-n*10), color[1]-(50-n*10), color[2]-(50-n*10)))
+                    if east :
+                        img2.putpixel((j*size+m,i*size+n), (color[0]-(200-(size-n)*40), color[1]-(200-(size-n)*40), color[2]-(200-(size-n)*40)))
+                    if not(north or south or west or east) :
+                        img2.putpixel((j*size+m,i*size+n), color)
     img2.show()
     img2.save("./resized_Images/" + name)
 
 resizer(image)
-
-# if (m == 0 and depthMap[i-1][j] > depthMap[i][j]) or (m == size-1 and ) or (n == 0 and) or (n == size-1 and) :
-#     img2.putpixel((j*size+m,i*size+n), (0, 0, 0))
--- depth_layer.py
+++ depth_layer.py
@@ -1,29 +1,30 @@
 from PIL import Image
-path = "./image_To_Edit/"
-image = Image.open(path + "imageTest.png")
 
 def getDepthLevels(img) :
     depthLevels = []
     for i in range(img.height) :
         for j in range(img.width) :
-            depth = img.getpixel((j,i))[:3]
+            depth = img.getpixel((i,j))[:3]
             if not depth in depthLevels :
                 depthLevels.append(depth)
-    depthLevels.sort(key=lambda x: (x[0]+x[1]+x[2]), reverse=True)
+    depthLevels.sort(key=lambda x: (x[0]+x[1]+x[2]))
     return depthLevels
 
 #print(getDepthLevels(image))
 
 
 def getDepthMap(img) :
-    depthMap = [[] for i in range(img.height)]
+    depthMap = [[] for i in range(img.height+2)]
     depthLevels = getDepthLevels(img)
-    for i in range(img.height) :
-        for j in range(img.width) :
-            color = img.getpixel((j,i))[:3]
-            depth = depthLevels.index(color)
-            depthMap[i].append(depth)
+    for i in range(img.height+2) :
+        for j in range(img.width+2) :
+            if j == 0 or j == img.width+1 or i == 0 or i == img.height+1 :
+                depthMap[i].append(-1)
+            else :
+                color = img.getpixel((j-1,i-1))[:3]
+                depth = depthLevels.index(color)
+                depthMap[i].append(depth)
     return depthMap
 
 
-#print(getDepthMap(image))
+# print(getDepthMap(Image.open("./depth_Layer/depthImage2.png")))
