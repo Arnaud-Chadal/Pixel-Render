6e4f44c8b0fbfc7409bd33f218f36ad5c42df8f6
Arnaud <chadal.arnaud2@gmail.com>
2023-12-08 21:10:05 +0100

    Start applyDepthOnImage function

--- apply_depth.py
+++ apply_depth.py
@@ -2,16 +2,25 @@ from depth_layer import *
 
 path = "./depth_Layer/"
 image = Image.open(path + "depthImage2.png")
+size = 10
 
-def resizer(img, size) :
+
+def applyDepthOnImage(x, y, depthMap) :
+    if x in [0] or y in [0] :
+        return (0,0,0)
+    else : return (255, 255, 255)
+
+
+def resizer(img) :
     name = "test.png"
+    depthMap = getDepthMap(img)
     img2 = Image.new('RGBA', (img.width*size, img.height*size), (255,255,255,1))
     for i in range(img.height) :
         for j in range(img.width) :
             for m in range(size) :
                 for n in range(size) :
-                    img2.putpixel((j*size+m,i*size+n), img.getpixel((j,i)))
+                    img2.putpixel((j*size+m,i*size+n), applyDepthOnImage(m,n,depthMap))
     img2.show()
     img2.save("./resized_Images/" + name)
 
-resizer(image, 1000)
+resizer(image)
