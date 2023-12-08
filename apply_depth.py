7f625654e7af6689aaa83cbe5c19088ca0307a79
Arnaud <chadal.arnaud2@gmail.com>
2023-12-08 21:29:19 +0100

    Add m/n loop to resizer function

--- apply_depth.py
+++ apply_depth.py
@@ -5,21 +5,18 @@ image = Image.open(path + "depthImage2.png")
 size = 10
 
 
-def applyDepthOnImage(x, y, depthMap) :
-    if x in [0] or y in [0] :
-        return (0,0,0)
-    else : return (255, 255, 255)
-
-
 def resizer(img) :
     name = "test.png"
     depthMap = getDepthMap(img)
     img2 = Image.new('RGBA', (img.width*size, img.height*size), (255,255,255,1))
     for i in range(img.height) :
         for j in range(img.width) :
+            color = img.getpixel((j,i))[:3]
             for m in range(size) :
                 for n in range(size) :
-                    img2.putpixel((j*size+m,i*size+n), applyDepthOnImage(m,n,depthMap))
+                    if m in [0] or n in [0] :
+                        img2.putpixel((j*size+m,i*size+n), (0, 0, 0))
+                    else : img2.putpixel((j*size+m,i*size+n), color)
     img2.show()
     img2.save("./resized_Images/" + name)
 
