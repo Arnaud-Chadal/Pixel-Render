008a1f3bf5ce7333b6f74b1ac08f502174dbec47
Arnaud <chadal.arnaud2@gmail.com>
2023-12-14 00:58:58 +0100

    fix i/j issue

--- apply_depth.py
+++ apply_depth.py
@@ -4,7 +4,7 @@ path = "./image_To_Edit/"
 pathLayer = "./depth_Layer/"
 image = Image.open(path + "imageTest.png")
 layer = Image.open(pathLayer + "depthImage2.png")
-size = 10
+size = 100
 
 
 def resizer(img) :
@@ -29,19 +29,19 @@ def resizer(img) :
             for m in range(size) :
                 for n in range(size) :
                     if north :
-                        if img2.getpixel((j*size+m,i*size+n))[0] > color[0]-(120-m*60) :
-                            img2.putpixel((j*size+m,i*size+n), (color[0]-(120-m*60), color[1]-(120-m*60), color[2]-(120-m*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-m*60) :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-m*60), color[1]-(180-m*60), color[2]-(180-m*60)))
                     if south :
-                        if img2.getpixel((j*size+m,i*size+n))[0] > color[0]-(120-(size-m)*60) :
-                            img2.putpixel((j*size+m,i*size+n), (color[0]-(120-(size-m)*60), color[1]-(120-(size-m)*60), color[2]-(120-(size-m)*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-(size-m)*60) :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-(size-m)*60), color[1]-(180-(size-m)*60), color[2]-(180-(size-m)*60)))
                     if west :
-                        if img2.getpixel((j*size+m,i*size+n))[0] > color[0]-(120-n*60) :
-                            img2.putpixel((j*size+m,i*size+n), (color[0]-(120-n*60), color[1]-(120-n*60), color[2]-(120-n*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-n*60) :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-n*60), color[1]-(180-n*60), color[2]-(180-n*60)))
                     if east :
-                        if img2.getpixel((j*size+m,i*size+n))[0] > color[0]-(120-(size-n)*60):
-                            img2.putpixel((j*size+m,i*size+n), (color[0]-(120-(size-n)*60), color[1]-(120-(size-n)*60), color[2]-(120-(size-n)*60)))
-                    if not(north or south or west or east) or img2.getpixel((j*size+m,i*size+n))[0] == 255 :
-                            img2.putpixel((j*size+m,i*size+n), color)
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-(size-n)*60):
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-(size-n)*60), color[1]-(180-(size-n)*60), color[2]-(180-(size-n)*60)))
+                    if not(north or south or west or east) or img2.getpixel((i*size+m,j*size+n))[0] == 255 :
+                            img2.putpixel((i*size+m,j*size+n), color)
     img2.show()
     img2.save("./resized_Images/" + name)
 
