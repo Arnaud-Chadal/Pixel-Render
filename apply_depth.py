86b8ffdeaa289a6bf00018390ae09cc44da75aaa
Arnaud <chadal.arnaud2@gmail.com>
2023-12-14 02:10:50 +0100

    Fix apply depth

--- apply_depth.py
+++ apply_depth.py
@@ -2,44 +2,36 @@ from depth_layer import *
 
 path = "./image_To_Edit/"
 pathLayer = "./depth_Layer/"
-image = Image.open(path + "imageTest.png")
-layer = Image.open(pathLayer + "depthImage2.png")
-size = 100
+image = Image.open(path + "apple.png")
+layer = Image.open(pathLayer + "apple_depth.png")
+size = 10
 
 
 def resizer(img) :
     name = "test.png"
     depthMap = getDepthMap(layer)
-    img2 = Image.new('RGBA', (img.width*size, img.height*size), "white")
+    img2 = Image.new('RGBA', (img.height*size, img.width*size), "white")
     for i in range(img.height) :
         for j in range(img.width) :
             color = img.getpixel((i, j))[:3]
-            north = False
-            south = False
-            west = False
-            east = False
-            if depthMap[i][j+1] > depthMap[i+1][j+1] :
-                north = True
-            if depthMap[i+2][j+1] > depthMap[i+1][j+1] :
-                south = True
-            if depthMap[i+1][j] > depthMap[i+1][j+1] :
-                west = True
-            if depthMap[i+1][j+2] > depthMap[i+1][j+1] :
-                east = True
+            north = depthMap[j+1][i] > depthMap[j+1][i+1]
+            south = depthMap[j+1][i+2] > depthMap[j+1][i+1]
+            west = depthMap[j][i+1] > depthMap[j+1][i+1]
+            east = depthMap[j+2][i+1] > depthMap[j+1][i+1]
             for m in range(size) :
                 for n in range(size) :
                     if north :
-                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-m*60) :
-                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-m*60), color[1]-(180-m*60), color[2]-(180-m*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-m*45) and 180-m*45 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-m*45), color[1]-(180-m*45), color[2]-(180-m*45)))
                     if south :
-                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-(size-m)*60) :
-                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-(size-m)*60), color[1]-(180-(size-m)*60), color[2]-(180-(size-m)*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-(size-m)*45) and 180-(size-m)*45 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-(size-m)*45), color[1]-(180-(size-m)*45), color[2]-(180-(size-m)*45)))
                     if west :
-                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-n*60) :
-                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-n*60), color[1]-(180-n*60), color[2]-(180-n*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-n*45) and 180-n*45 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-n*45), color[1]-(180-n*45), color[2]-(180-n*45)))
                     if east :
-                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-(size-n)*60):
-                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-(size-n)*60), color[1]-(180-(size-n)*60), color[2]-(180-(size-n)*60)))
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(180-(size-n)*45) and 180-(size-n)*45 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(180-(size-n)*45), color[1]-(180-(size-n)*45), color[2]-(180-(size-n)*45)))
                     if not(north or south or west or east) or img2.getpixel((i*size+m,j*size+n))[0] == 255 :
                             img2.putpixel((i*size+m,j*size+n), color)
     img2.show()
