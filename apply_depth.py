08bf8d74ae3b6994c29d8173951d719b2aff6e19
Arnaud <chadal.arnaud2@gmail.com>
2023-12-18 22:37:28 +0100

    Add angles

--- apply_depth.py
+++ apply_depth.py
@@ -18,6 +18,10 @@ def resizer(img) :
             south = depthMap[j+1][i+2] > depthMap[j+1][i+1]
             west = depthMap[j][i+1] > depthMap[j+1][i+1]
             east = depthMap[j+2][i+1] > depthMap[j+1][i+1]
+            northwest = depthMap[j][i] > depthMap[j+1][i+1]
+            northeast = depthMap[j+2][i] > depthMap[j+1][i+1]
+            southwest = depthMap[j][i+2] > depthMap[j+1][i+1]
+            southeast = depthMap[j+2][i+2] > depthMap[j+1][i+1]
             for m in range(size) :
                 for n in range(size) :
                     if north :
@@ -32,7 +36,19 @@ def resizer(img) :
                     if east :
                         if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(80-(size-n)*2) and 80-(size-n)*2 >= 0 :
                             img2.putpixel((i*size+m,j*size+n), (color[0]-(80-(size-n)*2), color[1]-(80-(size-n)*2), color[2]-(80-(size-n)*2)))
-                    if not(north or south or west or east) or img2.getpixel((i*size+m,j*size+n))[0] == 255 :
+                    if northwest :
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(80-max(m, n)*2) and 80-max(m, n)*2 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(80-max(m, n)*2), color[1]-(80-max(m, n)*2), color[2]-(80-max(m, n)*2)))
+                    if northeast :
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(80-max(m, (size-n))*2) and 80-max(m, (size-n))*2 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(80-max(m, (size-n))*2), color[1]-(80-max(m, (size-n))*2), color[2]-(80-max(m, (size-n))*2)))
+                    if southwest :
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(80-max((size-m), n)*2) and 80-max((size-m), n)*2 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(80-max((size-m), n)*2), color[1]-(80-max((size-m), n)*2), color[2]-(80-max((size-m), n)*2)))
+                    if southeast :
+                        if img2.getpixel((i*size+m,j*size+n))[0] > color[0]-(80-max((size-m), (size-n))*2) and 80-max((size-m), (size-n))*2 >= 0 :
+                            img2.putpixel((i*size+m,j*size+n), (color[0]-(80-max((size-m), (size-n))*2), color[1]-(80-max((size-m), (size-n))*2), color[2]-(80-max((size-m), (size-n))*2)))
+                    if not(north or south or west or east or northwest or northeast or southwest or southeast) or img2.getpixel((i*size+m,j*size+n))[0] == 255 :
                             img2.putpixel((i*size+m,j*size+n), color)
     img2.show()
     img2.save("./resized_Images/" + name)
